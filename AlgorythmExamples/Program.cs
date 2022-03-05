2d5842dd34875a3a7a9c1108e8ee018b2e38dd1f
amsdevelops <amsdevelops@gmail.com>
2022-03-05 22:21:32 +0300

    Added horse logic

--- AlgorythmExamples/Program.cs
+++ AlgorythmExamples/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        ulong mask = Chess.GetKingMoves(40);
+        ulong mask = Chess.GetHorseMoves(39);
         int cnt = Chess.popcnt(mask);
         Console.WriteLine(mask);
         Console.WriteLine(cnt);
